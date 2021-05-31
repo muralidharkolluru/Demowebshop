a864c3b949b52a4956f9c547542e132f586a8f4a
muralidharkolluru <muralidharkolluru@gmail.com>
2021-05-31 19:44:45 +0530

    projectiscreated

--- src/test/java/gluecode/LoginStepDefs.java
+++ src/test/java/gluecode/LoginStepDefs.java
@@ -1,7 +1,6 @@
 package gluecode;
 
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
